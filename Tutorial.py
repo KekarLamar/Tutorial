4b25a59f08782931af519ea8380203906cdad2ca
KekarLamar <161819817+KekarLamar@users.noreply.github.com>
2024-03-01 14:37:10 +0100

    Update Tutorial.py

--- Tutorial.py
+++ Tutorial.py
@@ -1,1 +1,19 @@
+import random
 
+def get_choices():
+
+    player_choice = input ("Enter a choice (rock, paper, scissors: ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = "rock"
+    choices = {"Player": player_choice , "Computer" : computer_choice}
+    return choices
+
+choices = get_choices()
+
+print (choices)
+
+def winnerPlayer(Player, Computer):
+    if (Player == Computer):
+        print("Empate")
+
+print("Fvck")
